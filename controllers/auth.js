a2dcb5952143f0302acb460ef2801fe6267e2161
Sakshi Chadha <sakshi.chadha@hyperverge.co>
2021-04-20 16:49:40 +0530

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,22 @@ const { validationResult } = require("express-validator");
 const User = require("../models/users");
 const Fin = require("../models/institution");
 const config = require("config");
-
+const {
+  FileSystemWallet,
+  Gateway,
+  X509WalletMixin,
+} = require("fabric-network");
+const path = require("path");
+const ccpPath = path.resolve(
+  __dirname,
+  "..",
+  "..",
+  "..",
+  "..",
+  "..",
+  "first-network",
+  "connection-org1.json"
+);
 
 const auth_token = async (req, res) => {
   const errors = validationResult(req);
@@ -12,7 +27,7 @@ const auth_token = async (req, res) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  
+
   const { email, password, user_type } = req.body;
 
   try {
@@ -49,7 +64,7 @@ const auth_token = async (req, res) => {
 };
 const user_by_token = async (req, res) => {
   try {
-    console.log("1")
+    console.log("1");
     const user = await User.findById(req.user.id).select("-password");
 
     res.json(user);
@@ -58,11 +73,10 @@ const user_by_token = async (req, res) => {
   }
 };
 
-
 const user_by_token2 = async (req, res) => {
   try {
-    console.log("1")
-    console.log("user_buttoenenken")
+    console.log("1");
+    console.log("user_buttoenenken");
     //console.log(req);
     const user = await Fin.findById(req.user.id).select("-password");
 
@@ -71,4 +85,61 @@ const user_by_token2 = async (req, res) => {
     console.log(err.message, "Server Error");
   }
 };
-module.exports = { auth_token, user_by_token,user_by_token2 };
+const approve_user = async (req, res) => {
+  try {
+    console.log("inside approve controller");
+    console.log(req.body);
+
+    //const user = await Fin.findById(req.user.id).select("-password");
+    try {
+      // Create a new file system based wallet for managing identities.
+      const walletPath = path.join(process.cwd() + "/../../", "wallet");
+      const wallet = new FileSystemWallet(walletPath);
+      console.log(`Wallet path: ${walletPath}`);
+
+      // Check to see if we've already enrolled the user.
+      const userExists = await wallet.exists("user1");
+      if (!userExists) {
+        console.log(
+          'An identity for the user "user1" does not exist in the wallet'
+        );
+        console.log("Run the registerUser.js application before retrying");
+        return;
+      }
+
+      // Create a new gateway for connecting to our peer node.
+      const gateway = new Gateway();
+      await gateway.connect(ccpPath, {
+        wallet,
+        identity: "user1",
+        discovery: { enabled: true, asLocalhost: true },
+      });
+
+      // Get the network (channel) our contract is deployed to.
+      const network = await gateway.getNetwork("mychannel");
+
+      // Get the contract from the network.
+      const contract = network.getContract("fabcar");
+
+      // Submit the specified transaction.
+      // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
+      // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
+      await contract.submitTransaction(
+        "changeCarOwner",
+        req.body.Key,
+        "Verified"
+      );
+      console.log("User has Been Verified");
+
+      // Disconnect from the gateway.
+      await gateway.disconnect();
+    } catch (error) {
+      console.error(`Failed to verify user: ${error}`);
+      process.exit(1);
+    }
+    // res.json(user);
+  } catch (err) {
+    console.log(err.message, "Server Error");
+  }
+};
+module.exports = { auth_token, user_by_token, user_by_token2, approve_user };
--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { check } = require('express-validator');
-const auth = require('../middleware/auth');
-const auth2 = require('../middleware/auth2');
-const {user_by_token,auth_token,user_by_token2} = require('../controllers/auth');
-const { ConnectionStates } = require('mongoose');
+const { check } = require("express-validator");
+const auth = require("../middleware/auth");
+const auth2 = require("../middleware/auth2");
+const {
+  user_by_token,
+  auth_token,
+  user_by_token2,
+  approve_user,
+} = require("../controllers/auth");
+const { ConnectionStates } = require("mongoose");
 
 // @route    GET api/auth
 // @desc     Get user by token
 // @access   Private
-router.get('/', auth,  (req, res) => {
-  console.log("1")
-  user_by_token(req,res);
-  console.log("end1")
+router.get("/", auth, (req, res) => {
+  console.log("1");
+  user_by_token(req, res);
+  console.log("end1");
 });
-
-router.get('/fin', auth2, (req, res) => {
-  console.log("123455")
-  user_by_token2(req,res);
-  console.log("end1123455")
+router.post("/approve", auth2, (req, res) => {
+  console.log("approve route");
+  //user_by_token(req,res);
+  approve_user(req, res);
+  console.log("end approve");
+});
+router.get("/fin", auth2, (req, res) => {
+  console.log("123455");
+  user_by_token2(req, res);
+  console.log("end1123455");
 });
 
 // @route    POST api/auth
 // @desc     Authenticate user & get token
 // @access   Public
 router.post(
-  '/',
-  check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Password is required').exists(),
-   (req, res) => {
-    
-    auth_token(req,res);
+  "/",
+  check("email", "Please include a valid email").isEmail(),
+  check("password", "Password is required").exists(),
+  (req, res) => {
+    auth_token(req, res);
   }
 );
 
